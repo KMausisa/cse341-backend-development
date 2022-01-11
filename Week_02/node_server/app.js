75c62f9d7b49667083b386374cd314eb2a13a9b6
= <=>
2022-01-10 22:44:32 -0700

    redirecting requests

--- Week_02/node_server/app.js
+++ Week_02/node_server/app.js
@@ -1,16 +1,26 @@
 // Import the http module
 const http = require('http');
+// Import the file system (fs) module
+const fs = require('fs');
 
 // Create a server
 const server = http.createServer((req, res) => {
     // Parse the URL
     const url = req.url;
+    // Parse the method
+    const method = req.method;
     if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         // Finish writing the response
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'Hello World!');
+        res.statusCode = 302; // 302 = re-direction
+        res.setHeader('Location', '/');
         return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
