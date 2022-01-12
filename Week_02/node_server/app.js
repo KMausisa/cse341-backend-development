f92f48efad360ba8b55ac0db3e4e51babc227358
= <=>
2022-01-11 18:46:33 -0700

    understanding the basics video course

--- Week_02/node_server/app.js
+++ Week_02/node_server/app.js
@@ -12,16 +12,30 @@ const server = http.createServer((req, res) => {
     if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         // Finish writing the response
         return res.end();
     }
     if (url === '/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'Hello World!');
-        res.statusCode = 302; // 302 = re-direction
-        res.setHeader('Location', '/');
-        return res.end();
+        const body = [];
+        // Get request data
+        req.on('data', (chunk) => {
+            // The listener receives a chunk of data
+            console.log(chunk);
+            // Place a new chunk of data in the body request
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            // Store the message in a constant
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, (err) => {
+                res.statusCode = 302; // 302 = re-direction
+                res.setHeader('Location', '/');
+                return res.end();
+            }); // writeFileSync blocks code execution until the file is created
+        });
     }
     res.setHeader('Content-Type', 'text/html');
     // Write data to the response
