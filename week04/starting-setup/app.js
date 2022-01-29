062a5ed4302fa2ad5c50f1809a4f6055aa3e49a7
= <=>
2022-01-29 15:09:46 -0700

    commit

---
+++ week04/starting-setup/app.js
@@ -0,0 +1,73 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const mongoose = require("mongoose");
+
+const errorController = require("./controllers/error");
+
+const User = require("./models/user");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findById("61f5a4c45e4e10ba2e131113")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+const cors = require('cors') // Place this with other requires (like 'path' and 'express')
+
+const corsOptions = {
+    origin: "https://infinite-oasis-68618.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+    family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://kmausisa:Kentimes0013@cse341cluster-3dwlw.mongodb.net/test?retryWrites=true&w=majority";
+                        
+
+mongoose
+  .connect(
+    MONGODB_URL, options
+  )
+  .then((result) => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: "Kendrick",
+          email: "test@gmail.com",
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
